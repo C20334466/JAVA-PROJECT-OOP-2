f897b4cfa63389a78d1a3eeb374cb0a0e474e307
C20334466 <101476113+C20334466@users.noreply.github.com>
2022-04-04 13:18:32 +0100

    File Proccessor Class
    This is the code where I open the file and read its contents

---
+++ FileProccessor.java
@@ -0,0 +1,51 @@
+package com.test.OOP;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class FileProccessor {
+
+		
+		//attributes
+		String fileName;   
+		File myFile;
+		
+		
+		//constructors
+		public FileProccessor(String fileName)
+		{
+				this.fileName = fileName;
+		}
+		
+		//open file method
+		public void openFile()
+		{
+			myFile = new File(fileName);	
+		}
+		
+		//read line method
+		public String readLines()
+		{
+				String line = "";  //right now we are just reading one line from the file
+				
+				try {
+					Scanner myScanner = new Scanner(myFile);
+					while(myScanner.hasNextLine())
+					{
+						line = myScanner.nextLine();
+						System.out.println(line );
+					}
+					myScanner.close();
+				//error checking
+				} catch (FileNotFoundException e) { 
+					e.printStackTrace();
+					System.out.println("Error Caught "+ e.getMessage());
+				}
+				return line;
+		}
+		
+	}
+
+
