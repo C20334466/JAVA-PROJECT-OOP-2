419082e9cb59099352c48180203aa3e2296eadb0
C20334466 <101476113+C20334466@users.noreply.github.com>
2022-04-05 17:14:07 +0100

    Update and rename ControlScreen.java to Control.java

---
+++ java/Control.java
@@ -0,0 +1,13 @@
+package searchengine;
+
+
+public class Control {
+	
+	public static void main(String[] args) {
+		
+		SearchEngine searchEngine = new SearchEngine();
+		FileProccessor filingCheck = new FileProccessor();
+	
+	}
+
+}
