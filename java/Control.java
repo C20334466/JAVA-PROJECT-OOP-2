1145d0273a1635244e5d3d3bae3f4bea20395565
C20334466 <101476113+C20334466@users.noreply.github.com>
2022-04-06 11:11:33 +0100

    Update Control.java
    updated commenting

--- java/Control.java
+++ java/Control.java
@@ -4,9 +4,11 @@ package searchengine;
 public class Control {
 	
 	public static void main(String[] args) {
-		
+		//open gui
 		SearchEngine searchEngine = new SearchEngine();
+		//opens and reads files
 		FileProccessor filingCheck = new FileProccessor();
+		
 	
 	}
 
